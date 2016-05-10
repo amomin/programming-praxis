d4c3fd135f2446829d3939eaf5befa13f39f3986
Al Momin <al.momin@outlook.com>
2016-05-10 19:53:12 -0400

    some additions to concatenate nn (after reading solutions)

--- haskell/ConcatenateNNTimes.hs
+++ haskell/ConcatenateNNTimes.hs
@@ -1,6 +1,8 @@
 -- Concatenate N N Times
 -- May 10, 2016
 -- 
+-- https://programmingpraxis.com/2016/05/10/concatenate-n-n-times/
+--
 -- Your task is to write a program that calculates the number that is
 -- concatenated to itself the number of times as the number is
 -- (that's hard to say).
@@ -11,6 +13,8 @@
 
 module ConcatenateNNTimes where
 
+import Data.Bits
+
 sol1 :: Integer -> String
 sol1 m = helper m m ""
     where
@@ -19,3 +23,26 @@ sol1 m = helper m m ""
             | k < 0     = "Error"
             | k < 1     = sofar
             | otherwise =  helper n (k-1) (sofar ++ (show n))
+
+-- Using binary rep to build in log many steps
+repeatStrNTimes :: String -> Integer -> String
+repeatStrNTimes str k
+    | k < 0             = "Error"
+    | k < 1             = ""
+    | (mod k 2) == 1    = prev ++ prev ++ str
+    | otherwise         = prev ++ prev
+        where
+            prev = repeatStrNTimes str (k `shiftR` 1)
+
+-- FWIW a more generic version of the above
+repeatShowableNTimes :: (Show a) => a -> Integer -> String
+repeatShowableNTimes str k
+    | k < 0             = "Error"
+    | k < 1             = ""
+    | (mod k 2) == 1    = prev ++ prev ++ (show str)
+    | otherwise         = prev ++ prev
+        where
+            prev = repeatShowableNTimes str (k `shiftR` 1)
+
+sol2 :: Integer -> String
+sol2 m = repeatStrNTimes (show m) m
--- python/ConcatenateNNTimes.py
+++ python/ConcatenateNNTimes.py
@@ -1,6 +1,8 @@
 ################# Concatenate N N Times #####################
 ##################### May 10, 2016  #########################
 #
+# https://programmingpraxis.com/2016/05/10/concatenate-n-n-times/
+#
 # Your task is to write a program that calculates the number
 # that is concatenated to itself the number of times as the
 # number is (that's hard to say).
@@ -14,6 +16,23 @@ def sol1(N):
     for i in range(0,N):
         ans = ans + str(N)
     return ans
+
+# Using base 2 rep of N to build it in log many steps
+# (but still concatenating long strings so maybe not as efficient as may seem)
+
+def repeatStrNTimes(_str, N):
+    if N < 0:
+        "ERROR"
+    if N < 1:
+        return ""
+    prev = repeatStrNTimes(_str, (N >> 1))
+    if (N % 2 == 1):
+        return prev + prev + _str
+    else:
+        return prev + prev
+
+def sol2(N):
+    return repeatStrNTimes(str(N), N)
 
 def test1():
     if "" == sol1(0):
@@ -44,9 +63,43 @@ def test1():
     else:
         print "Fails on input 123"
 
+def test2():
+    if "" == sol2(0):
+        print "Passes on input 0"
+    else:
+        print "Fails on input 0"
+    if "1" == sol2(1):
+        print "Passes on input 1"
+    else:
+        print "Fails on input 1"
+    if "88888888" == sol2(8):
+        print "Passes on input 8"
+    else:
+        print "Fails on input 8"
+    if "1111111111111111111111" == sol2(11):
+        print "Passes on input 11"
+    else:
+        print "Fails on input 11"
+    if "121212121212121212121212" == sol2(12):
+        print "Passes on input 12"
+    else:
+        print "Fails on input 12"
+    ten123 = "123123123123123123123123123123"
+    hundred123 = ten123 + ten123 + ten123 + ten123 + ten123 + ten123 + ten123 + ten123 + ten123 + ten123
+    ans123 = hundred123 + ten123 + ten123 + "123123123"
+    if ans123 == sol2(123):
+        print "Passes on input 123"
+    else:
+        print "Fails on input 123"
+
 if __name__ == '__main__':
-    print "Some Tests:"
+    print "Some Tests for sol1:"
     test1()
-    print "Examples"
+    print "Some Tests for sol2:"
+    test2()
+    print "Examples of sol1"
     for i in range(0,20):
         print "Output for N=", i, "is", sol1(i)
+    print "Examples of sol2"
+    for i in range(0,20):
+        print "Output for N=", i, "is", sol2(i)
