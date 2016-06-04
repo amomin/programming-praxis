82590efc66a690e0e77c258371de37ad709da844
Al Momin <al.momin@outlook.com>
2016-06-04 20:07:40 +0000

    Learn a new language from May 31st, 2016

---
+++ scala/LearnANewLanguage.scala
@@ -0,0 +1,26 @@
+/**
+ * Learn a new language
+ *
+ * https://programmingpraxis.com/2016/05/31/learn-a-new-language-4/
+ *
+ * Implementing quick sort (or something like it in a functional spirit)
+ * in Scala.
+ */
+
+object QuickishSort {
+  def sort(arr: Array[Int]): Array[Int] = {
+    if (arr.length < 2) {
+      return arr;
+    }
+    var x = arr(0);
+    return sort(arr.filter(_ < x)) ++ arr.filter(_ == x) ++ sort(arr.filter(_ > x))
+  }
+  
+  def main(args: Array[String]) {
+    println("Hello world");
+    var sorted =sort(Array[Int](12,45,65,1,45,23,76,4,36,17,72,14,64));
+    for (i <- sorted) {
+      println(i);
+    }
+  }
+}
